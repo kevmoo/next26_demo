46f3e3061bd36ea55ce6c554f7caa1cf645b87b9
Kevin Moore <kevmoo@google.com>
2026-01-28 13:34:34 -0800

    more fun

--- app/lib/src/auth_gate_widget.dart
+++ app/lib/src/auth_gate_widget.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../main.dart';
+import 'home_page_widget.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
