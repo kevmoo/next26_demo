a0f61597c294bd908cd9e870f017291aa5fade81
Kevin Moore <kevmoo@google.com>
2026-02-06 16:05:51 -0800

    server: drop auth_guard

--- server/lib/main.dart
+++ server/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:firebase_functions/firebase_functions.dart';
 import 'package:next26_shared/next26_shared.dart';
 
-import 'src/auth_guard.dart';
 import 'src/storage_controller.dart';
 
 void main(List<String> args) async {
@@ -32,7 +31,10 @@ void main(List<String> args) async {
     );
 
     firebase.https.onCall(name: 'increment', (request, response) async {
-      final userId = await authGuard(request);
+      final userId =
+          request.auth?.uid ??
+          (throw UnauthenticatedError('User is not signed-in!'));
+
       final result = await storageController.increment(userId);
 
       return CallableResult({
--- server/lib/src/auth_guard.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_functions/firebase_functions.dart';
-
-Future<String> authGuard(CallableRequest request) async {
-  if (request.auth?.uid != null) {
-    return request.auth!.uid;
-  }
-
-  throw UnauthenticatedError('User is not signed-in!');
-}
