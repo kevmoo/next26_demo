1a6317732e7237322448e95a31563164ef95ab0e
Kevin Moore <kevmoo@google.com>
2026-02-26 17:17:14 -0800

    Move app bits to src

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'app.dart';
 import 'firebase_options.dart';
+import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
