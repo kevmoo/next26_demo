75451b2c93b0b9adfa35656925029f3bb5118a3d
kevmoo <kevmoo@google.com>
2026-03-26 11:32:37 -0700

    multi_count: better auth!

--- multi_counter/server/bin/server.dart
+++ multi_counter/server/bin/server.dart
@@ -1,4 +1,4 @@
-import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:dart_firebase_admin/dart_firebase_admin.dart';
 import 'package:firebase_functions/firebase_functions.dart';
 import 'package:multi_counter_server/src/storage_controller.dart';
 import 'package:multi_counter_shared/multi_counter_shared.dart';
@@ -9,7 +9,7 @@ void main(List<String> args) async {
   await fireUp(args, (firebase) {
     firebase.https.onRequest(name: incrementCallable, (request) async {
       final userId =
-          _authIdFromRequest(request) ??
+          await _authIdFromRequest(request) ??
           (throw UnauthenticatedError('User is not signed-in!'));
 
       await storageController.increment(userId);
@@ -21,21 +21,21 @@ void main(List<String> args) async {
   });
 }
 
-String? _authIdFromRequest(Request request) {
+Future<String?> _authIdFromRequest(Request request) async {
   final idToken = request.headers['Authorization']?.split(' ').last;
   if (idToken == null) {
     return null;
   }
 
-  Object? payload;
   try {
-    payload = JWT.decode(idToken).payload;
+    final decoded = await _app.auth().verifyIdToken(idToken);
+    print('Decoded: ${decoded.authTime} ${decoded.uid} ${decoded.email}');
+    return decoded.uid;
   } catch (_) {
     return null;
   }
-
-  return switch (payload) {
-    {'user_id': final String id} => id,
-    _ => null,
-  };
 }
+
+FirebaseApp get _app => __app ??= FirebaseApp.initializeApp();
+
+FirebaseApp? __app;
