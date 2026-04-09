a57008f985e7af0bd0e1256e41715c5d6a56b993
kevmoo <kevmoo@google.com>
2026-04-09 10:50:15 -0700

    latest deps

--- multi_counter/server/bin/server.dart
+++ multi_counter/server/bin/server.dart
@@ -4,7 +4,7 @@ import 'package:multi_counter_shared/multi_counter_shared.dart';
 
 void main(List<String> args) async {
   await fireUp(args, (firebase) async {
-    final storageController = StorageController(firebase.firestoreAdmin);
+    final storageController = StorageController(firebase.adminApp.firestore());
 
     firebase.https.onCall(
       name: incrementCallable,
